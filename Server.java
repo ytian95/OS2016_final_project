4a8a5fbc9bf5e272583bdc8c9cc88b32910303d9
emancuso <mancusoelyse@gmail.com>
2016-11-12 16:17:11 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,119 +2,125 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+public class Server implements Runnable {
+    private int port;
+    Socket connectionSocket;
 
-public class Server {
-	private int port;
+    public void run() {
+	try {
+	    BufferedReader input = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+	    DataOutputStream output = new DataOutputStream(connectionSocket.getOutputStream());
+	    http_handler(input, output);
+	} catch (IOException e) {
+	    System.out.println(e.getMessage());
+	}
+    }
+    
+    public static void main(String args[]){
+	ServerSocket serverSocket;
+	try {
+	    System.out.println("Trying to bind to localhost on port 1234...");
+	    serverSocket = new ServerSocket(1234);
+	    System.out.println("Listening");
+	} catch(Exception e) { //catch any errors and print errors to gui
+	    System.out.println("\nFatal Error:" + e.getMessage());
+	    return;
+	}
+	while (true) {
+	    System.out.println("\nReady, Waiting for requests...\n");
+	    try {
+		Socket socket = serverSocket.accept();
+		System.out.println("Connected");
+		new Thread(new Server(socket)).start();
+		InetAddress client = socket.getInetAddress();
+		System.out.println(client.getHostName() + " connected to server.\n");
+	    } catch (Exception e) { 
+		System.out.println("\nError:" + e.getMessage());
+	    }
+	}
+    }
 
-	public static void main(String args[]){
-		new Server(1234);
+    public Server(Socket socket) {
+	port = socket.getLocalPort();
+	System.out.println("Port:" + port);
+	connectionSocket = socket;
+    }
+    
+    private void http_handler(BufferedReader input, DataOutputStream output) {
+	//Two types of request we can handle:
+	//GET /index.html HTTP/1.0
+	//HEAD /index.html HTTP/1.0
+	String path = "";
+
+	try {
+		String[] request = input.readLine().split(" ");
+		path = request[1].substring(1);
+		output.writeBytes(construct_http_header(200, 5));
+		BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+		System.out.println("path opening: " + path);
+		String line="";
+		while ((line=br.readLine())!=null) {
+			output.writeUTF(line);
+			System.out.println("line: "+line);
+		}
+		output.writeUTF("requested file name :"+path);
+		output.close(); 
+		br.close();
 	}
 
-	public Server(int listen_port) {
-		port = listen_port;
-		ServerSocket serversocket = null; 
-		try {
-			System.out.println("Trying to bind to localhost on port " + Integer.toString(port) + "...");
-			serversocket = new ServerSocket(port);
-		} catch (Exception e) { //catch any errors and print errors to gui
-			System.out.println("\nFatal Error:" + e.getMessage());
-			return;
-		}
-		while (true) {
-			System.out.println("\nReady, Waiting for requests...\n");
-			try {
-				Socket connectionsocket = serversocket.accept();
-				InetAddress client = connectionsocket.getInetAddress();
-				System.out.println(client.getHostName() + " connected to server.\n");
-				BufferedReader input = new BufferedReader(new InputStreamReader(connectionsocket.getInputStream()));
-				DataOutputStream output = new DataOutputStream(connectionsocket.getOutputStream());
-				httpHandler(input, output);
-			} catch (Exception e) { 
-				System.out.println("\nError:" + e.getMessage());
-			}
-		} 
-	}
-	private void httpHandler(BufferedReader input, DataOutputStream output) {
-		//Two types of request we can handle:
-		//GET /index.html HTTP/1.0
-		//HEAD /index.html HTTP/1.0
-		String path = "";
-		
-		try {
-			String[] request = input.readLine().split(" ");
-			
-			path = request[1].substring(1);
-			output.writeBytes(construct_http_header(200, 5));
-			BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-			System.out.println("path opening: " + path);
-			
-			String line="";
-			while ((line=br.readLine())!=null) {
-				output.writeUTF(line);
-				System.out.println("line: "+line);
-			}
-			output.writeUTF("requested file name :"+path);
-			output.close(); 
-			br.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+    private String construct_http_header(int return_code, int file_type) {
+	String s = "HTTP/1.0 ";
+	switch (return_code) {
+	case 200:
+	    s = s + "200 OK";
+	    break;
+	case 400:
+	    s = s + "400 Bad Request";
+	    break;
+	case 403:
+	    s = s + "403 Forbidden";
+	    break;
+	case 404:
+	    s = s + "404 Not Found";
+	    break;
+	case 500:
+	    s = s + "500 Internal Server Error";
+	    break;
+	case 501:
+	    s = s + "501 Not Implemented";
+	    break;
+	default:
+	    s = s + "You've reached the error page for the error page! You win!";
+	    break;
 	}
 
-	private String construct_http_header(int return_code, int file_type) {
-		String s = "HTTP/1.0 ";
-		switch (return_code) {
-		case 200:
-			s = s + "200 OK";
-			break;
-		case 400:
-			s = s + "400 Bad Request";
-			break;
-		case 403:
-			s = s + "403 Forbidden";
-			break;
-		case 404:
-			s = s + "404 Not Found";
-			break;
-		case 500:
-			s = s + "500 Internal Server Error";
-			break;
-		case 501:
-			s = s + "501 Not Implemented";
-			break;
-		default:
-			s = s + "You've reached the error page for the error page! You win!";
-			break;
-		}
+	s = s + "\r\n"; 
+	s = s + "Connection: close\r\n"; 
+	s = s + "Server: SmithOperatingSystemsCourse v0\r\n"; //server name
 
-		s = s + "\r\n"; 
-		s = s + "Connection: close\r\n"; 
-		s = s + "Server: SmithOperatingSystemsCourse v0\r\n"; //server name
-
-		switch (file_type) {
-		case 0:
-			break;
-		case 1:
-			s = s + "Content-Type: image/jpeg\r\n";
-			break;
-		case 2:
-			s = s + "Content-Type: image/gif\r\n";
-		case 3:
-			s = s + "Content-Type: application/x-zip-compressed\r\n";
-		default:
-			s = s + "Content-Type: text/html\r\n";
-			break;
-		}
-		s = s + "\r\n"; 
-		return s;
+	switch (file_type) {
+	case 0:
+	    break;
+	case 1:
+	    s = s + "Content-Type: image/jpeg\r\n";
+	    break;
+	case 2:
+	    s = s + "Content-Type: image/gif\r\n";
+	case 3:
+	    s = s + "Content-Type: application/x-zip-compressed\r\n";
+	default:
+	    s = s + "Content-Type: text/html\r\n";
+	    break;
 	}
+	s = s + "\r\n"; 
+	return s;
+    }
 
-}
+} 
